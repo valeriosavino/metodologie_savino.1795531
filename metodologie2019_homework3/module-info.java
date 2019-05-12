c07a89ab5a9cdf7d17735e42fbfe5b6ce95e613e
valeriosavino <48855421+valeriosavino@users.noreply.github.com>
2019-05-12 22:25:18 +0200

    Add files via upload

---
+++ metodologie2019_homework3/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * @author savinovalerio
+ *
+ */
+module metodologie2019_homework3 {
+	exports it.uniroma1.metodologie2019.hw3;
+
+	requires junit;
+	requires org.junit.jupiter.api;
+}
